e1b4cc9e295172389643206a6d01228353c9a611
IC-huanglc <haunglc_work@163.com>
2023-11-29 14:33:41 +0800

    clean up bins

--- designs/socv/rtl/rtl_lpw/opencores/oc_spi.f
+++
@@ -1,6 +0,0 @@
-
-+incdir+$UVM_REF_HOME/designs/socv/rtl/rtl_lpw/opencores/spi/rtl
-
-spi/rtl/spi_clgen.v
-spi/rtl/spi_shift.v
-spi/rtl/spi_top.v
--- designs/socv/rtl/rtl_lpw/rom_subsystem/rtl/rom_subsystem.f
+++
@@ -1,3 +0,0 @@
-rom_response_gen.v
-rom_sram.v
-rom_subsystem.v
